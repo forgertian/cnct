2176dce406b1947b53f18f0d254f5aa9943bded3
JasonTian <291025986@.com>
2019-11-20 21:51:19 +0800

    第六次提交：修正外层工程pom中错误，添加user微服务

---
+++ cnct-user/cnct-user-interface/src/main/java/com/cnct/pojo/User.java
@@ -0,0 +1,58 @@
+package com.cnct.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_user")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @NotBlank(message = "手机号不能为空！")
+  @Pattern(regexp = "^1[35678]\\d{9}$", message = "手机号格式不正确")
+  private String phone;
+  @JsonIgnore
+  private String password;
+  private String name;
+  private Long gender;
+  private String email;
+  private String companyName;
+
+  @Pattern(regexp = "^1[35678]\\d{9}$", message = "手机号格式不正确")
+  private String refPhone;
+  private Long roleId;
+  private Long isLicompany;
+  private Long isIntlicompany;
+  private Long isShipping;
+  private Long isRailTransport;
+  private Long isAirTransport;
+  private String otherWay;
+  private String weichatNo;
+  private String qqNo;
+  private String image;
+  private Long province;
+  private String provinceName;
+  private Long city;
+  private String cityName;
+  private Long district;
+  private String districtName;
+  private String address;
+  private Long goodsType;
+  private Long isStart;
+  private java.sql.Date createDate;
+  private Long isVip;
+  private java.sql.Date vipEndTime;
+  private Long bannedStatus;
+}
--- cnct-user/cnct-user-server/src/main/java/com/cnct/CnctUserServer.java
+++ cnct-user/cnct-user-server/src/main/java/com/cnct/CnctUserServer.java
@@ -2,21 +2,15 @@ package com.cnct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
-/**
- *
- */
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
 @MapperScan("com.cnct.mapper")
-public class BusinessApplication {
-
-    public static void main(String[] args){
-        SpringApplication.run(BusinessApplication.class,args);
+public class CnctUserServer {
+    public static void main(String[] args) {
+        SpringApplication.run(CnctUserServer.class,args);
     }
-
 }
